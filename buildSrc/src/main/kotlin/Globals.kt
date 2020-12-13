b0be18067427c2c404b183f2465c2f5ed3ab94a9
BoD <BoD@JRAF.org>
2020-12-13 18:19:48 +0100

    Update dependencies

--- buildSrc/src/main/kotlin/Globals.kt
+++ buildSrc/src/main/kotlin/Globals.kt
@@ -1,10 +1,10 @@
 object Versions {
-    const val GRADLE = "6.4.1"
-    const val KOTLIN = "1.3.72"
-    const val KTOR = "1.3.2"
+    const val GRADLE = "6.7.1"
+    const val KOTLIN = "1.4.21"
+    const val KTOR = "1.4.3"
     const val KTOR_GRAPHQL = "1.0.0"
     const val LOGBACK = "1.2.3"
-    const val BEN_MANES_VERSIONS_PLUGIN = "0.28.0"
-    const val KLIBQONTO = "2.1.0"
-    const val JACKSON_KOTLIN = "2.11.0"
+    const val BEN_MANES_VERSIONS_PLUGIN = "0.36.0"
+    const val KLIBQONTO = "2.2.0"
+    const val JACKSON_KOTLIN = "2.12.0"
 }
